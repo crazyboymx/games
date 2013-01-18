c407d83f63e320f85300ddbc2607dd5bf9716032
Pingan Yi <pingany@opera.com>
2013-01-18 16:45:08 +0800

    Make background suitable for screen

--- HelloJump/Classes/BackgroundLayer.cpp
+++ HelloJump/Classes/BackgroundLayer.cpp
@@ -6,6 +6,10 @@ bool BackgroundLayer::init()
 {
 	CCLayer::init();
 	CCSprite* background = CocosUtils::getSprite("background.png");
+
+	CCSize screenSize = CocosUtils::getScreenSize();
+	background->setScaleX(screenSize.width / background->getContentSize().width);
+	background->setScaleY(screenSize.height / background->getContentSize().height);
 	background->setAnchorPoint(ccp(0, 0));
 	background->setPosition(ccp(0, 0));
 	addChild(background);
